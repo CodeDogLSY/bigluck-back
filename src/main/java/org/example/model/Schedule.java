81f6a9ea1ee344a80d5ca6c704478d0c8e1eae6c
CodeDogLSY <liushiyang2016@gmail.com>
2024-03-25 16:14:17 +0800

    排版调整

--- src/main/java/org/example/model/Schedule.java
+++ src/main/java/org/example/model/Schedule.java
@@ -2,12 +2,19 @@ package org.example.model;
 
 public class Schedule {
     private String id;
-    private String employee_id;
     private String schedule_date;
+    private String employee_id;
+    private String employee_name;
 
     public Schedule(String id, String employee_id, String schedule_date) {
         this.id = id;
         this.employee_id = employee_id;
+        this.schedule_date = schedule_date;
+    }
+    public Schedule(String id, String employee_id, String employee_name, String schedule_date) {
+        this.id = id;
+        this.employee_id = employee_id;
+        this.employee_name = employee_name;
         this.schedule_date = schedule_date;
     }
 
@@ -33,5 +40,13 @@ public class Schedule {
 
     public void setSchedule_date(String schedule_date) {
         this.schedule_date = schedule_date;
+    }
+
+    public String getEmployee_name() {
+        return employee_name;
+    }
+
+    public void setEmployee_name(String employee_name) {
+        this.employee_name = employee_name;
     }
 }
